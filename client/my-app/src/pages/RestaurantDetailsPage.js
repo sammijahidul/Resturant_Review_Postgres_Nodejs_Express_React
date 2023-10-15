fc13f91709dfb58bfbe7b6a4e3c0e4b738b0d64e
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2023-10-15 20:40:12 +0600

    just created restaurant details componenet

--- client/my-app/src/pages/RestaurantDetailsPage.js
+++ client/my-app/src/pages/RestaurantDetailsPage.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { RestaurantsContext } from '../context/RestaurantsContext';
+import RestaurantFinder from '../apis/RestaurantFinder';
 
 const RestaurantDetailsPage = () => {
+  const { id } = useParams();
+  const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurant(response.data.data.restaurant);
+      } 
+      catch (error) {
+        console.log(error);
+      }      
+    };
+    fetchData();
+
+  }, [])
   return (
     <div>
-      RestaurantDetailsPage
+      {selectedRestaurant && selectedRestaurant.name}
     </div>
   )
 }
